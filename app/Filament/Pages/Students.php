fa9c480efb3dd605046f7780b21b016a52935afd
NabihahFatiny <nurnabihahfatiny545@gmail.com>
2026-03-13 10:10:46 +0800

    added student icon

--- app/Filament/Pages/Students.php
+++ app/Filament/Pages/Students.php
@@ -3,9 +3,13 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
+use BackedEnum; // Add this line at the top
 
 class Students extends Page
 {
+    // Update this line to include the full type list
+    protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-user-group';
+
     protected static ?string $navigationLabel = 'Students';
 
     protected static ?string $title = 'Students';
